541c9ab1c9aaef0b27f525fe3131a34fc2e32657
leafloose <andy.wong@ualberta.ca>
2019-11-10 18:55:32 -0700

    P300 working? Needs hardware users to test

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -12,6 +12,7 @@ public class Ball : MonoBehaviour
   public Rigidbody2D rb;
   public Rigidbody2D hook;
   public GameObject nextBall;
+  public bool useOneKey = true;
 
   public float releaseTime = 0.15f;
   private float currentDragDistance = 2.25f;
@@ -28,15 +29,16 @@ public class Ball : MonoBehaviour
   public Image healthbar;
   public Canvas healthCanvas;
 
-  private Vector3 launch_direction {
+  private Vector3 launch_direction
+  {
     get
     {
       return new Vector3((float)(Math.Cos(angle)), (float)(Math.Sin(angle)), 0).normalized;
     }
   }
 
-  private double originX = -5.31f;
-  private double originY = -1.95f;
+  private double originX;
+  private double originY;
 
   private LineRenderer lineRenderer;
   private static int traj_nsteps = 20;
@@ -60,6 +62,29 @@ public class Ball : MonoBehaviour
   }
 
 
+  public void move(int action)
+  {
+    if (!hasShot)
+    {
+      switch (action)
+      {
+        case 0: { currentDragDistance = (currentDragDistance <= maxDragDistance) ? currentDragDistance + 0.15f : currentDragDistance; break; }
+        case 1: { currentDragDistance = (currentDragDistance >= minDragDistance) ? currentDragDistance - 0.15f : currentDragDistance; break; }
+        case 2: { angle = (angle >= 5 * Math.PI / 7) ? 0.1f + angle : angle; break; }
+        case 3: { angle = (angle <= 9 * Math.PI / 7) ? -0.1f + angle : angle; break; }
+        case 4:
+          {
+            hasShot = true;
+            rb.isKinematic = false;
+            rb.velocity = calculate_velocity();
+            StartCoroutine(Release());
+            break;
+          }
+      }
+      rb.MovePosition((Vector3)hook.position + (currentDragDistance * launch_direction));
+    }
+  }
+
   private void Start()
   {
     rb.isKinematic = true;
@@ -67,55 +92,78 @@ public class Ball : MonoBehaviour
     lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
     lineRenderer.widthMultiplier = 0.07f;
     lineRenderer.positionCount = traj_nsteps;
+    originX = hook.position.x;
+    originY = hook.position.y;
   }
 
   void Update()
   {
     healthbar.fillAmount = currentDragDistance / maxDragDistance;
-    if (Input.GetKey("space"))
+
+    if (useOneKey)
     {
-      if (aiming)
+      if (Input.GetKey(KeyCode.Space))
       {
-        // Setting the trajectory of the ball
-        direction = (angle >= 9 * Math.PI / 7) ? false : direction;
-        direction = (angle <= 5 * Math.PI / 7) ? true : direction;
-        angle = direction ? angle + 0.01f : angle - 0.01f;
+        if (aiming)
+        {
+          // Setting the trajectory of the ball
+          direction = (angle <= 5 * Math.PI / 7) ? true : direction;
+          direction = (angle >= 9 * Math.PI / 7) ? false : direction;
+          angle = direction ? angle + 0.01f : angle - 0.01f;
 
-        Vector3 originPosition = new Vector3((float)originX, (float)originY, 0);
-        Vector3 offset = new Vector3((float)(Math.Cos(angle)), (float)(Math.Sin(angle)), 0);
-        offset.Normalize();
-        Vector3 desiredPosition = originPosition + (currentDragDistance * offset);
-        transform.position = desiredPosition;
+          transform.position = (Vector3)hook.position + (currentDragDistance * launch_direction);
+        }
+        else
+        {
+          // Setting the power of the ball
+          direction = (currentDragDistance >= maxDragDistance) ? false : direction;
+          direction = (currentDragDistance <= minDragDistance) ? true : direction;
+          currentDragDistance = direction ? currentDragDistance + 0.025f : currentDragDistance - 0.025f;
 
+          transform.position = (Vector3)hook.position + (currentDragDistance * launch_direction);
+        }
       }
-      else 
-      {
-        // Setting the power of the ball
-        direction = (currentDragDistance >= maxDragDistance) ? false : direction;
-        direction = (currentDragDistance <= minDragDistance) ? true : direction;
-        currentDragDistance = direction ? currentDragDistance + 0.025f : currentDragDistance - 0.025f;
 
-        Vector3 originPosition = new Vector3((float)originX, (float)originY, 0);
-        Vector3 desiredPosition = originPosition + (currentDragDistance * launch_direction);
-        transform.position = desiredPosition;
+      // Change from aiming to power to launch. 
+      if (Input.GetKeyUp(KeyCode.Space))
+      {
+        if (aiming)
+        {
+          aiming = false;
+        }
+        else if (hasShot == false)
+        {
+          hasShot = true;
+          rb.isKinematic = false;
+          rb.velocity = calculate_velocity();
+          StartCoroutine(Release());
+        }
+      }
+    }
+    else
+    {
+      if (Input.GetKeyDown(KeyCode.UpArrow))
+      {
+        move(0);
+      }
+      else if (Input.GetKeyDown(KeyCode.DownArrow))
+      {
+        move(1);
+      }
+      else if (Input.GetKeyDown(KeyCode.LeftArrow))
+      {
+        move(2);
+      }
+      else if (Input.GetKeyDown(KeyCode.RightArrow))
+      {
+        move(3);
+      }
+      else if (Input.GetKeyDown(KeyCode.Space))
+      {
+        move(4);
       }
     }
 
-    // Change from aiming to power to launch. 
-    if (Input.GetKeyUp(KeyCode.Space))
-    {
-      if (aiming)
-      {
-        aiming = false;
-      }
-      else if (hasShot == false) 
-      {
-        hasShot = true;
-        rb.isKinematic = false;
-        rb.velocity = calculate_velocity();
-        StartCoroutine(Release());
-      }
-    }
 
     // For handling the trajectory line
     if (rb.isKinematic)
@@ -125,34 +173,36 @@ public class Ball : MonoBehaviour
         calculate_traj();
         lineRenderer.SetPosition(i, traj_arr[i]);
       }
-      
+
     }
     else
     {
       lineRenderer.enabled = false;
     }
 
-    IEnumerator Release()
-    {
-      healthCanvas.enabled = false;
-      // For launching the ball, and moving control to the next ball. 
-      UIManager.BallsUsed++;
-      yield return new WaitForSeconds(releaseTime);
-      this.enabled = false;
 
-      yield return new WaitForSeconds(2f);
-      if (nextBall != null && UIManager.EnemiesAlive != 0)
+  }
+  IEnumerator Release()
+  {
+    healthCanvas.enabled = false;
+    // For launching the ball, and moving control to the next ball. 
+    UIManager.BallsUsed++;
+    yield return new WaitForSeconds(releaseTime);
+    this.enabled = false;
+
+    yield return new WaitForSeconds(2f);
+    if (nextBall != null && UIManager.EnemiesAlive != 0)
+    {
+      nextBall.SetActive(true);
+    }
+    else
+    {
+      yield return new WaitForSeconds(3f);
+      if (UIManager.EnemiesAlive != 0)
       {
-        nextBall.SetActive(true);
-      }
-      else
-      {
-        yield return new WaitForSeconds(3f);
-        if (UIManager.EnemiesAlive != 0)
-        {
-          UIManager.levelFail();
-        }
+        UIManager.levelFail();
       }
     }
   }
 }
+
--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -6,79 +6,97 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+  public GameObject HUD;
+  public static int score = 0;
+  public static int EnemiesAlive = 0;
+  public static int BallsUsed = 0;
 
-    public static int score = 0;
-    public static int EnemiesAlive = 0;
-    public static int BallsUsed = 0;
+  public GameObject completeScreen;
 
-    public GameObject completeScreen;
+  public Text scoreTextWin;
+  public Text ballText;
 
-    public Text scoreTextWin;
-    public Text ballText;
+  public GameObject loseScreen;
 
-    public GameObject loseScreen;
+  public Text restartTimerLose;
 
-    public Text restartTimerLose;
+  public LevelManager LevelManager;
 
-    public LevelManager LevelManager;
+  public GameObject LevelStartScreen;
+  public Text levelText;
 
-    public GameObject LevelStartScreen;
-    public Text levelText;
+  void Start()
+  {
+    levelText.text = SceneManager.GetActiveScene().name;
+    LevelStartScreen.SetActive(true);
+    StartCoroutine(StartHUD());
 
-    void Start() {
-        levelText.text = SceneManager.GetActiveScene().name;
-        LevelStartScreen.SetActive(true);
+  }
+
+  IEnumerator StartHUD()
+  {
+    yield return new WaitForSeconds(1.5f);
+    HUD.SetActive(true);
+  }
+
+  public void levelComplete()
+  {
+
+    StartCoroutine(FinishWin());
+    HUD.SetActive(false);
+
+  }
+
+  public void levelFail()
+  {
+
+    StartCoroutine(FinishLose());
+    HUD.SetActive(false);
+
+  }
+
+  public void restartLevel()
+  {
+
+    score = 0;
+    EnemiesAlive = 0;
+    BallsUsed = 0;
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+  }
+
+  public void nextLevel()
+  {
+
+    score = 0;
+    EnemiesAlive = 0;
+    BallsUsed = 0;
+    LevelManager.nextLevel();
+
+  }
+
+  IEnumerator FinishWin()
+  {
+    yield return new WaitForSeconds(1.3f);
+    scoreTextWin.text = (score).ToString("");
+    ballText.text = (3 - BallsUsed).ToString("");
+    completeScreen.SetActive(true);
+
+    yield return new WaitForSeconds(3f);
+
+    nextLevel();
+  }
+
+  IEnumerator FinishLose()
+  {
+    loseScreen.SetActive(true);
+
+    for (int i = 4; i > 0; i--)
+    {
+      restartTimerLose.text = (i).ToString("");
+      yield return new WaitForSeconds(1f);
     }
 
-    public void levelComplete() {
-
-        StartCoroutine(FinishWin());
-
-    }
-
-    public void levelFail() {
-
-        StartCoroutine(FinishLose());
-
-    }
-
-    public void restartLevel() {
-
-        score = 0;
-        EnemiesAlive = 0;
-        BallsUsed = 0;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-
-    }
-
-    public void nextLevel() {
-        
-        score = 0;
-        EnemiesAlive = 0;
-        BallsUsed = 0;
-        LevelManager.nextLevel();
-
-    }
-
-    IEnumerator FinishWin () {
-        yield return new WaitForSeconds(1.3f);
-        scoreTextWin.text = (score).ToString("");
-        ballText.text = (3 - BallsUsed).ToString("");
-        completeScreen.SetActive(true);
-
-        yield return new WaitForSeconds(3f);
-
-        nextLevel();
-    }
-
-    IEnumerator FinishLose () {
-        loseScreen.SetActive(true);
-
-        for (int i = 4; i > 0; i--) {
-            restartTimerLose.text = (i).ToString("");
-            yield return new WaitForSeconds(1f);
-        }
-
-        restartLevel();
-    }
+    restartLevel();
+  }
 }
